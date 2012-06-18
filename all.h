8db6185bc6dd6daf87341aa3f882ee6d74a0b04d
Jan Funke <funke@ini.ch>
2012-06-18 13:11:38 -0400

    added all.h to replace former pipeline.h

---
+++ all.h
@@ -0,0 +1,9 @@
+#ifndef PIPELINE_H__
+#define PIPELINE_H__
+
+#include "pipeline/ProcessNode.h"
+#include "pipeline/SimpleProcessNode.h"
+#include "pipeline/signals/all.h"
+
+#endif // PIPELINE_H__
+
